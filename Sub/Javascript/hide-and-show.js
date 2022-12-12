262ea1e860295400e47ee772efe7dfd7a0f9eb1e
marivita126 <89694409+marivita126@users.noreply.github.com>
2022-12-13 03:11:34 +0900

    1213_01

---
+++ Sub/Javascript/hide-and-show.js
@@ -0,0 +1,29 @@
+
+function hide_and_show_sec1() {
+  var x = document.getElementById("section01");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hide_and_show_sec2() {
+  var x = document.getElementById("section02");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function hide_and_show_sec3() {
+  var x = document.getElementById("section03");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
